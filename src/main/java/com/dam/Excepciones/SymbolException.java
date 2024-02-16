c9429e5c6bb6e2ce2bf75e3b35fb5c3aebfc8c43
UrosPower <felix.aldea@educa.madrid.org>
2024-02-16 17:04:33 +0100

    Añadidas excepciones a los diversos metodos y sus tests correspondientes

---
+++ src/main/java/com/dam/Excepciones/SymbolException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dam.Excepciones;
+
+/**
+ *
+ * @author dev
+ */
+public class SymbolException extends Exception {
+    public SymbolException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/com/dam/gestorpalabras/GestorPalabras.java
+++ src/main/java/com/dam/gestorpalabras/GestorPalabras.java
@@ -3,7 +3,7 @@
  */
 package com.dam.gestorpalabras;
 
-import com.dam.Excepciones.TooShortWordException;
+import com.dam.Excepciones.*;
 
 /**
  *
@@ -11,38 +11,74 @@ import com.dam.Excepciones.TooShortWordException;
  */
 public class GestorPalabras {
 
-    public boolean esPalindromo(String palabra) throws TooShortWordException {
+    public boolean esPalindromo(String palabra) throws TooShortWordException, NoNumberException, SymbolException {
         String palabraFormateada = palabra.replaceAll("\\s+", "").toLowerCase();
+
         if (palabra.length() <= 2) {
             throw new TooShortWordException("Palabra demasiado corta, por favor introduce una palabra de más longitud");
+        } else if (recorrerPalabra(palabra)) {
+            throw new SymbolException("No están admitidos símbolos");
         } else {
-            palabraFormateada = palabraFormateada.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u');
-            for (int i = 0; i < palabraFormateada.length() / 2; i++) {
-                if (palabraFormateada.charAt(i) != palabraFormateada.charAt(palabraFormateada.length() - i - 1)) {
-                    return false;
+            for (int i = 0; i < palabraFormateada.length(); i++) {
+                if (Character.isDigit(palabra.charAt(i))) {
+                    throw new NoNumberException("No vale un número, tienes que poner una palabra");
                 }
+            }
+        }
+        palabraFormateada = palabraFormateada.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u');
+        for (int i = 0; i < palabraFormateada.length() / 2; i++) {
+            if (palabraFormateada.charAt(i) != palabraFormateada.charAt(palabraFormateada.length() - i - 1)) {
+                return false;
             }
         }
         return true;
     }
 
-    public int contarVocales(String palabra) {
+    public int contarVocales(String palabra) throws NoNumberException, SymbolException {
         int contador = 0;
+        boolean p;
         for (int i = 0; i < palabra.length(); i++) {
             char caracter = Character.toLowerCase(palabra.charAt(i));
-            if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u' || caracter == 'á' || caracter == 'é' || caracter == 'í' || caracter == 'ó' || caracter == 'ú') {
-                contador++;
+            p = Character.isDigit(palabra.charAt(i));
+            if (p == true) {
+                throw new NoNumberException("No vale un numero, tienes que poner una palabra");
+            } else if (recorrerPalabra(palabra)) {
+                throw new SymbolException("No están admitidos símbolos");
+            } else {
+                if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u' || caracter == 'á' || caracter == 'é' || caracter == 'í' || caracter == 'ó' || caracter == 'ú') {
+                    contador++;
+                }
             }
         }
         return contador;
     }
 
-    public String invertirPalabra(String palabra) {
+    public String invertirPalabra(String palabra) throws NoNumberException, SymbolException {
         StringBuilder resultado = new StringBuilder();
+        boolean p;
         for (int i = palabra.length() - 1; i >= 0; i--) {
-            resultado.append(palabra.charAt(i));
+            p = Character.isDigit(palabra.charAt(i));
+            if (p == true) {
+                throw new NoNumberException("No vale un numero, tienes que poner una palabra");
+            } else if (recorrerPalabra(palabra)) {
+                throw new SymbolException("No están admitidos símbolos");
+
+            } else {
+                resultado.append(palabra.charAt(i));
+            }
         }
         return resultado.toString();
     }
 
+    public static boolean recorrerPalabra(String palabra) {
+        Character p;
+        for (int i = 0; i < palabra.length(); i++) {
+            p = palabra.charAt(i);
+            if (p.equals('@') || p.equals('%') || p.equals('/') || p.equals('*') || p.equals('+')
+                    || p.equals('=') || p.equals('~') || p.equals('$') || p.equals('&')) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
